461790e52b32de9cc5132ee6067e57c1143f5e8f
AshifAli007 <theashifkhan@gmail.com>
2025-04-16 17:23:24 -0400

    new features added working till here

--- ingredia-frontend/src/App.js
+++ ingredia-frontend/src/App.js
@@ -1,7 +1,5 @@
-// App.js
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { ClerkProvider, RedirectToSignIn, useClerk, useUser } from '@clerk/clerk-react';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import SavedRecipes from './components/SavedRecipes';
@@ -9,7 +7,9 @@ import ShoppingList from './components/ShoppingList';
 import { SignedIn, SignedOut, SignIn, SignUp } from "@clerk/clerk-react";
 import img1 from './Assets/Img/img1.jpeg';
 import { getRecipes, getFilteredRecipes } from './api';
-import './App.css'
+import './App.css'; // Ensure this file contains the necessary styles
+import Sidebar from "./components/Sidebar/Sidebar";
+import Ingredients from "./components/Ingredients/Ingredients";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -34,8 +34,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-
-
         <SignedOut>
           <div className="signed-out-container">
             <div className="image-container">
@@ -56,23 +54,19 @@ function App() {
           </div>
         </SignedOut>
         <SignedIn>
-          <nav>
-            <a href="/">Home</a>
-            <a href="/saved-recipes">My Recipes</a>
-            <a href="/shopping-list">Shopping List</a>
-          </nav>
-          <Routes>
-            <Route exact path="/" Component={() => <RecipeList recipes={filteredRecipes} onFilterChange={handleFilterChange} />} />
-            {/* <Route exact path="/auth" Component={Auth} /> */}
-            <Route path="/recipe/:id" Component={RecipeDetails} />
-            <Route path="/saved-recipes" Component={SavedRecipes} />
-            <Route path="/shopping-list" Component={ShoppingList} />
-          </Routes>
+          <div className="main-layout">
+            <Sidebar />
+            <div className="content">
+              <Routes>
+                <Route exact path="/" Component={() => <RecipeList recipes={filteredRecipes} onFilterChange={handleFilterChange} />} />
+                <Route path="/ingredients" Component={Ingredients} />
+                <Route path="/recipe/:id" Component={RecipeDetails} />
+                <Route path="/saved-recipes" Component={SavedRecipes} />
+                <Route path="/shopping-list" Component={ShoppingList} />
+              </Routes>
+            </div>
+          </div>
         </SignedIn>
-
-
-
-
       </div>
     </Router>
   );
